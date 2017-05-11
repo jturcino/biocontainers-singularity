5236876465bc4949ec493c1daac7b830327fc41e
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2017-05-11 11:50:02 -0500

    added token update every 350 container jobs submitted

--- resync.sh
+++ resync.sh
@@ -1,33 +1,46 @@
 
 syncfile="resync_containers.txt"
 
-echo 'GETTING CONTAINER NAMES AND VERSIONS...'
+function refresh_token() {
+	local newtoken=`echo $(auth-tokens-refresh -S) | rev | cut -d ' ' -f 1 | rev`
+	echo "$newtoken"
+}
+
 # get containers to resync; store in resync_images.txt
+echo "GETTING CONTAINER NAMES AND VERSIONS..."
 ./get-resync-containers.py -f $syncfile
+
 # refresh and get Agave token
-token=`echo $(auth-tokens-refresh -S) | rev | cut -d ' ' -f 1 | rev`
+token=$(refresh_token)
 
 # read in resync_images.txt
 containers=`cat $syncfile | awk '{print $1}'`
 tags=`cat $syncfile | awk '{print $2}'`
 num_containers=`echo $containers | wc -w`
 
-echo 'BEGINNING TO RESYNC CONTAINERS...'
 # resync containers 
+echo "BEGINNING TO RESYNC CONTAINERS..."
 for i in $(seq 1 $num_containers); do
+
 	# get container, tag, and system
 	container=`echo $containers | cut -d ' ' -f $i`
 	tag=`echo $tags | cut -d ' ' -f $i`
 	system="jfonner-jetstream-docker$(( $i%2+2 ))"
-	# submit to sync-container.py
-	./sync-container.py -c $container -t $tag -s $system -z $token
+
+	# submit to build-container.py
+	./build-container.py -c $container -t $tag -s $system -z $token
+
+	# refresh token every 350 containers
+	if [ $(( $i % 350 )) -eq 0 ]; then 
+		token=$(refresh_token)
+	fi
 
 	# sleep 30s
 	sleep 30
 done
 
-echo 'REMOVING STORAGE FILE...'
 # remove syncfile
+echo "REMOVING STORAGE FILE..."
 rm $syncfile
 
-echo 'RESYNC COMPLETE'
+echo "RESYNC COMPLETE"
