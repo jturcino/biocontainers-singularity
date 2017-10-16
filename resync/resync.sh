53d5fad6d3db24b3899f671cbe4917e8bc009da0
Jacquelyn Turcinovic <jturcino@c455-014.stampede2.tacc.utexas.edu>
2017-10-15 19:05:43 -0500

    added command line args

--- resync/resync.sh
+++ resync/resync.sh
@@ -1,24 +1,68 @@
 #!/usr/bin/env bash
 
-syncfile="resync_containers.txt"
-system="jfonner-jetstream-docker3"
-
 function refresh_token() {
     local newtoken=`echo $(auth-tokens-refresh -S) | rev | cut -d ' ' -f 1 | rev`
     echo "$newtoken"
 }
 
-# get containers to resync; store in resync_images.txt
-echo "GETTING CONTAINER NAMES AND VERSIONS..."
-./get-resync-containers.py -f $syncfile
+#syncfile="resync_containers.txt"
+system="jfonner-jetstream-docker3"
+tag="latest"
+HELP="
+This script orchestrates the generation of multiple singularity images using the 
+Agave docker-to-singularity app. When run in its default state (no options provided), 
+it updates the repository of quay.io biocontainers images on Stampede2. If the user 
+has their own list of images to generate, they can be provided in a formatted text 
+file: one line per container, with container and tag separated by a space.
 
-# refresh and get Agave token
-token=$(refresh_token)
+Usage: ./resync.sh [OPTIONS]
 
-# read in resync_images.txt
+Options:
+  -h, --help		show this help message and exit
+  -f, --syncfile 	name of the syncfile from which to read containers and tags
+  -z, --token		token with which to submit jobs to the system
+  -s, --system		execution system with docker-to-singularity app
+"
+
+# argparse
+while [[ $# -gt 1 ]]; do
+    key="$1"
+    case $key in
+        -f|--syncfile) shift;
+            syncfile="$1"
+            shift ;;
+        -z|--token) shift;
+            token="$1"
+            shift ;;
+        -s|--system) shift;
+            system="$1"
+            shift ;;
+        *)
+            echo "$HELP"
+            exit 0
+    esac
+done
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo "$HELP"
+    exit 0
+fi
+
+# if syncfile not present, get containers to resync
+if ! [ -e "$syncfile" ]; then
+    echo "GETTING CONTAINER NAMES AND VERSIONS..."
+    ./get-resync-containers.py -f $syncfile
+fi
+
+# read in resync_containers.txt
 containers=`cat $syncfile | awk '{print $1}'`
 tags=`cat $syncfile | awk '{print $2}'`
 num_containers=`echo $containers | wc -w`
+
+# refresh Agave token if not provided
+if [ -z "$token" ]; then
+    token=$(refresh_token)
+fi
 
 # resync containers 
 echo "BEGINNING TO RESYNC CONTAINERS..."
@@ -31,17 +75,11 @@ for i in $(seq 1 $num_containers); do
     # submit to build-container.py
     ./build-container.py -c $container -t $tag -s $system -z $token
 
-    # refresh token every 350 containers
+    # refresh token every 100 containers
     if [ $(( $i % 100 )) -eq 0 ]; then 
         token=$(refresh_token)
     fi
-
-    # sleep 30s
     sleep 120
 done
-
-## remove syncfile
-#echo "REMOVING STORAGE FILE..."
-#rm $syncfile
 
 echo "RESYNC COMPLETE"
